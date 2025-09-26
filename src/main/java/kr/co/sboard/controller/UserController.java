b5efdb4b346755540d07461cddd6b08cd7b228bc
sundae1014 <link8430@gmail.com>
2025-09-26 12:21:10 +0900

    add 0.0.1

--- src/main/java/kr/co/sboard/controller/UserController.java
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,10 +1,15 @@
 package kr.co.sboard.controller;
 
+import kr.co.sboard.config.AppInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+
 
     @GetMapping("/user/info")
     public String info(){
